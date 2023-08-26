ef40933a4f25aedb1958e6407c20e277d607bf82
ttyS3 <ttys3.rust@gmail.com>
2023-08-26 13:08:21 +0800

    chore: allow generate for Unsupported SDK type like go or rust

--- scripts/generate_sdk.py
+++ scripts/generate_sdk.py
@@ -87,9 +87,11 @@ def generate_sdk(sanitized_spec_path, sdk_type, output_path):
         template_override_path = os.path.join(os.path.dirname(
             __file__), "../sdk-template-overrides/typescript-axios")
         os.system(
-            f"openapi-generator generate -i {sanitized_spec_path} -g typescript-axios -o {output_path} -p supportsES6=true -t {template_override_path}")
+            f"openapi-generator-cli generate -i {sanitized_spec_path} -g typescript-axios -o {output_path} -p supportsES6=true -t {template_override_path}")
     else:
-        print(f"Unsupported SDK type {sdk_type}, skipping SDK generation")
+        print(f"Unsupported SDK type {sdk_type}, but still do SDK generation")
+        os.system(
+            f"openapi-generator-cli generate -i {sanitized_spec_path} -g {sdk_type} -o {output_path}")
 
 
 parser = argparse.ArgumentParser()
